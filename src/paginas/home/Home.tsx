46025cc1d07995997b0396472ad3e2528e25e445
Daniela Xavier <danielamxsoares@gmail.com>
2024-02-14 12:04:09 -0300

    primeiro componente em react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import './Home.css'
+
+function Home() {
+    return (
+     <>  
+     <h1 className= "titulo">I love my cats</h1>
+     <img className= "img" src="https://img.freepik.com/fotos-premium/dois-gatinhos-fofos-em-coracoes-amorosos-romanticos-generative-ai_791316-12460.jpg" alt="cats" />
+     </>
+    )
+}
+
+export default Home;
